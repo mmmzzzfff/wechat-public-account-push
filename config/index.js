2129e24d7222cb314e59b9338225593641d24da2
mmmzzzfff <112232237+mmmzzzfff@users.noreply.github.com>
2022-08-27 13:37:45 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,11 +25,10 @@ export const config = {
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
 // 天气：{{weather.DATA}}  
-// 最低气温: {{min_temperature.DATA}}  
-// 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
-// {{birthday_message.DATA}}
+// 最低温度: {{min_temperature.DATA}}  
+// 最高温度: {{max_temperature.DATA}}  
+// 今天是我们在一起的第{{love_day.DATA}}天
+// 距离我们觉觉的生日还有{{birthday_message.DATA}}天
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
