f3394ad8b2c3150cbece8f9e050521110d65fe38
mmmzzzfff <112232237+mmmzzzfff@users.noreply.github.com>
2022-08-27 13:49:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "XVh9G7N-WbDEc4xaW-lAjXBF5tFgGnIgrEeD7CVOTaw",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["oOK4m6ovG2YL2GGQCgLysIJQLbKo"],
+    user: ["oOK4m6ovG2YL2GGQCgLysIJQLbKo","oOK4m6gGqdsLlHDGFzQjif1l1RMw"],
      
     // 信息配置
     // 所在省份
